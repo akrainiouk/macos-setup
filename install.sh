345969cfaebb418da378c1889bd03ff81f5e838f
Alexei Krainiouk <akrainio@adobe.com>
2020-04-10 09:48:27 -0700

    Instead of symlinking .bashrc and .bash_profile requires customizing the existing files

--- install.sh
+++ install.sh
@@ -1,30 +1,75 @@
 #!/bin/bash
-timeStamp=$(date "+%Y-%m-%d_%H.%M.%S")
+
+targetRef="$HOME/lib/macos-setup"
+targetPath="$HOME/lib/versions/macos-setup-SNAPSHOT"
+self="$(realpath --canonicalize-existing "$0")"
+sourcePath="$(dirname "$self")"
+timeStamp="$(date "+%Y-%m-%d_%H_%M.%S")"
 backupDir="/tmp/config-backup-$timeStamp"
+
+error() {
+  echo "$1"
+  exit 1
+}
+
+bold() {
+  echo -e "\033[1m$1\033[0m"
+}
+
+underscore() {
+  echo -e "\033[4m$1\033[0m"
+}
+
+#echo "self:      [$self]"
+#echo "srcPath:   [$sourcePath]"
+#echo "dstPath:   [$targetPath]"
+#echo "backupDir: [$backupDir]"
+
+! test -e "$targetRef"  || error "Path [$targetRef] already exists"
+! test -e "$targetPath" || error "Path [$targetPath] already exists"
+
 echo "Saving original setup into [$backupDir]"
+mkdir -p "$backupDir"
 
-mkdir -p $backupDir
+mkdir -p "$(dirname "$targetPath")"
+ln -s "$sourcePath" "$targetPath"
+ln -s "$targetPath" "$targetRef"
 
-installFile() {
-  src="$(realpath $1)"
+installSymlink() {
+  local src dst
+  src="$(realpath "$1")"
   dst="$2"
   case $dst in
-    /*) 
+  /*)
     echo "dst [$dst] must be relative to user home"
     exit 1
     ;;
   esac
-  if [[ -e $HOME/$dst ]]
+  if [[ -e "$HOME/$dst" || -L "$HOME/$dst" ]]
   then
-    mkdir -p $backupDir/$(dirname $dst)
-    mv $HOME/$dst $backupDir/$dst
+    mkdir -p "$backupDir/$(dirname "$dst")"
+    mv "$HOME/$dst" "$backupDir/$dst"
   fi
-  ln -s $src $HOME/$dst
+  ln -s "$src" "$HOME/$dst"
 }
 
 echo "Installing symlinks to startup scripts..."
-installFile bashrc .bashrc
-installFile bash_profile .bash_profile
-installFile gitconfig .gitconfig
-installFile vimrc .vimrc
+for file in $(gfind $sourcePath/config -type f -printf "%P\n")
+do
+  # shellcheck disable=SC2088
+  echo "~/.$file -> $targetRef/config/$file"
+  installSymlink "$targetRef/config/$file" ".$file"
+done
 
+echo "
+Installation complete. Add the following lines to your startup
+scripts right before any exiting customizations:
+"
+# shellcheck disable=SC2088
+underscore "~/.bashrc":
+bold "    source $targetRef/startup/bashrc"
+echo
+# shellcheck disable=SC2088
+underscore "~/.bash_profile":
+bold "    source $targetRef/startup/bash_profile"
+echo
